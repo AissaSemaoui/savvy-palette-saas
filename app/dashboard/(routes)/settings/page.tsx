5c0dd749b10bc65ce82d3916a82541bad4eb760a
AissaSemaoui <semaoui0347@gmail.com>
2023-08-24 11:36:20 +0100

    remove profileForm component

--- app/dashboard/(routes)/settings/page.tsx
+++ app/dashboard/(routes)/settings/page.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 
-import ProfileForm from "@/lib/settings/components/profile-form";
 import { UserProfile } from "@clerk/nextjs";
 
 const SettingsPage = () => {
   return (
     <main className="sm:container !max-w-4xl" id="settings-page">
       <h1 className="text-xl md:text-4xl font-medium mb-6">Settings</h1>
-      {/* <ProfileForm /> */}
       <UserProfile />
     </main>
   );
